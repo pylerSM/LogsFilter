c48a438f183721e167d158518ea912d33202713e
pylerSM <pyler@azet.sk>
2015-02-17 18:58:50 +0100

    Initial release

---
+++ src/com/pyler/logsfilter/LogsFilter.java
@@ -0,0 +1,70 @@
+package com.pyler.logsfilter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import android.util.Log;
+import de.robv.android.xposed.IXposedHookZygoteInit;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XSharedPreferences;
+import de.robv.android.xposed.XposedBridge;
+
+public class LogsFilter implements IXposedHookZygoteInit {
+	public static final String LOGS_FILTER_MANAGE = "logs_filter_manage";
+	public static final String LOGS_FILTER = "logs_filter";
+	public static final String LOGS = "logs";
+	public static final String EMPTY_LOG = "";
+	public XSharedPreferences prefs;
+	public XC_MethodHook logsHook;
+
+	@Override
+	public void initZygote(StartupParam startupParam) throws Throwable {
+		prefs = new XSharedPreferences(LogsFilter.class.getPackage().getName());
+		prefs.makeWorldReadable();
+
+		logsHook = new XC_MethodHook() {
+			@Override
+			protected void beforeHookedMethod(MethodHookParam param)
+					throws Throwable {
+				prefs.reload();
+				boolean logs = prefs.getBoolean(LOGS, true);
+				boolean logsFilter = prefs.getBoolean(LOGS_FILTER, false);
+				if (!(param.args[1] instanceof String)) {
+					return;
+				}
+				if (!logs) {
+					param.args[1] = EMPTY_LOG;
+					return;
+				}
+				if (!logsFilter) {
+					return;
+				}
+				String log = (String) param.args[1];
+				param.args[1] = filterLog(log);
+
+			}
+		};
+
+		XposedBridge.hookAllMethods(Log.class, "d", logsHook);
+		XposedBridge.hookAllMethods(Log.class, "e", logsHook);
+		XposedBridge.hookAllMethods(Log.class, "w", logsHook);
+		XposedBridge.hookAllMethods(Log.class, "i", logsHook);
+		XposedBridge.hookAllMethods(Log.class, "wtf", logsHook);
+	}
+
+	public String filterLog(String log) {
+		String filteredLog = log;
+		prefs.reload();
+		Set<String> items = prefs.getStringSet(LOGS_FILTER_MANAGE,
+				new HashSet<String>());
+		if (items.isEmpty()) {
+			return filteredLog;
+		}
+		for (String item : items) {
+			if (filteredLog.contains(item)) {
+				filteredLog = filteredLog.replaceAll(item, "");
+			}
+		}
+		return filteredLog;
+	}
+}
---
+++ src/com/pyler/logsfilter/Settings.java
@@ -0,0 +1,81 @@
+package com.pyler.logsfilter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.MultiSelectListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+
+public class Settings extends PreferenceActivity {
+	public static final String LOGS_FILTER_ADD = "logs_filter_add";
+	public static final String LOGS_FILTER_MANAGE = "logs_filter_manage";
+	public static final String LOGS_FILTER_CLEAR = "logs_filter_clear";
+
+	@SuppressWarnings("deprecation")
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		getPreferenceManager().setSharedPreferencesMode(MODE_WORLD_READABLE);
+		addPreferencesFromResource(R.xml.settings);
+		EditTextPreference addLogsFilter = (EditTextPreference) findPreference(LOGS_FILTER_ADD);
+		Preference clearLogsFilter = findPreference(LOGS_FILTER_CLEAR);
+		addLogsFilter
+				.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+					@Override
+					public boolean onPreferenceChange(Preference preference,
+							Object newValue) {
+						String text = (String) newValue;
+						addLogsFilter(text);
+						reloadLogsFilter();
+						return true;
+					}
+				});
+		clearLogsFilter
+				.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+
+					@Override
+					public boolean onPreferenceClick(Preference preference) {
+						clearLogsFilter();
+						reloadLogsFilter();
+						return false;
+					}
+				});
+		reloadLogsFilter();
+	}
+
+	@SuppressWarnings("deprecation")
+	public void reloadLogsFilter() {
+		MultiSelectListPreference logsFilter = (MultiSelectListPreference) findPreference("logs_filter_manage");
+		SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(getApplicationContext());
+		Set<String> items = prefs.getStringSet(LOGS_FILTER_MANAGE,
+				new HashSet<String>());
+		CharSequence[] logsFilterItems = items.toArray(new CharSequence[items
+				.size()]);
+		logsFilter.setEntries(logsFilterItems);
+		logsFilter.setEntryValues(logsFilterItems);
+	}
+
+	public void addLogsFilter(String text) {
+		SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(getApplicationContext());
+		Set<String> logsFilterItems = prefs.getStringSet(LOGS_FILTER_MANAGE,
+				new HashSet<String>());
+		logsFilterItems.add(text);
+		SharedPreferences.Editor prefsEditor = prefs.edit();
+		prefsEditor.remove(LOGS_FILTER_MANAGE).apply();
+		prefsEditor.putStringSet(LOGS_FILTER_MANAGE, logsFilterItems).apply();
+	}
+
+	public void clearLogsFilter() {
+		SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(getApplicationContext());
+		SharedPreferences.Editor prefsEditor = prefs.edit();
+		prefsEditor.remove(LOGS_FILTER_MANAGE).apply();
+	}
+}
